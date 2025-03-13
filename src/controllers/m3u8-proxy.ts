f0192e6f60208c6745b6d4b6d73762f06a660141
julz <owanojulius742002@gmail.com>
2025-03-14 07:36:10 +0800

    Back to added regex in m3u8proxy

--- src/controllers/m3u8-proxy.ts
+++ src/controllers/m3u8-proxy.ts
@@ -1,53 +1,82 @@
 import axios from "axios";
 import { Request, Response } from "express";
-
-const allowedExtensions = ['.ts', '.png', '.jpg', '.webp', '.ico', '.html', '.js', '.css', '.txt'];
+import { editUrl } from "../utils/helper";
 
 export const m3u8Proxy = async (req: Request, res: Response) => {
   try {
     const url = req.query.url as string;
-
-    if (!url) return res.status(400).json("url is required");
-
-    if (allowedExtensions.some(ext => url.endsWith(ext))) {
-      const response = await axios.get(url, {
-        responseType: 'stream',
-        headers: {
-          Accept: "*/*",
-          Referer: "https://megacloud.club/",
-        }
-      });
-      const headers = Object.fromEntries(response.headers as any);
-      res.cacheControl = { maxAge: headers['cache-control'] }
-      res.set(headers);
-      return response.data.pipe(res);
-    }
-
     const response = await axios.get(url, {
       responseType: 'text',
-      headers: {
-        Accept: "*/*",
-        Referer: "https://megacloud.club/",
-      }
     });
-    const headers = Object.fromEntries(response.headers as any);
+    const editedUrl = editUrl(url);
+    const proxyUrl = `${process.env.BASE_URL}/m3u8-quality-proxy?url=${editedUrl}`;
     const originalContent = response.data as string;
-    const modifiedContent = originalContent.split("\n").map((line) => {
-      if (line.endsWith('.m3u8') || line.endsWith('.ts')) {
-        return `m3u8-proxy?url=${url.replace(/[^/]+$/, "")}${line}`;
-      }
+    const updatedContent = originalContent
+      .split('\n')
+      .map((line) => {
+        if (line.startsWith('#EXT-X-MEDIA') && line.includes('URI=')) {
+          return line.replace(/URI="(.*?)"/, (match, p1) => {
+            // Update the URI with the proxy URL
+            console.log({ match, p1 })
+            return `URI="${proxyUrl}${p1}"`;
+          });
+        }
 
-      if (allowedExtensions.some(ext => line.endsWith(ext))) {
-        return `m3u8-proxy?url=${line}`
-      }
+        if (line.startsWith('#EXT') || line.startsWith('http')) return line;
+        return line ? `${proxyUrl}${line}` : '';
+      })
+      .join('\n');
 
-      return line;
-    }).join("\n");
-
-    res.cacheControl = { maxAge: headers['cache-control'] }
-    res.set(headers);
-    return res.status(200).send(modifiedContent);
+    res.set({
+      'Content-Type': 'application/vnd.apple.mpegurl',
+      'Cache-Control': response.headers['cache-control'],
+    });
+    res.status(200).send(updatedContent);
   } catch (error: any) {
-    console.log(error.message)
+    console.log(error.message);
   }
-}
+};
+
+export const m3u8QualityProxy = async (req: Request, res: Response) => {
+  try {
+    const url = req.query.url as string;
+    const response = await axios.get(url, {
+      responseType: 'text',
+    });
+    const editedUrl = editUrl(url);
+    const proxyUrl = `${process.env.BASE_URL}/m3u8-segment-proxy?url=${editedUrl}`;
+    const originalContent = response.data as string;
+    const updatedContent = originalContent
+      .split('\n')
+      .map(line => {
+        if (line.startsWith('#EXT')) return line;
+        return line ? `${proxyUrl}${line}` : '';
+      })
+      .join('\n');
+
+    res.set({
+      'Content-Type': 'application/vnd.apple.mpegurl',
+      'Cache-Control': response.headers['cache-control'],
+    });
+    res.status(200).send(updatedContent);
+  } catch (error: any) {
+    console.log(error.message);
+  }
+};
+
+export const m3u8SegmentProxy = async (req: Request, res: Response) => {
+  try {
+    const url = req.query.url as string;
+    const response = await axios.get(url, {
+      responseType: 'stream',
+    });
+
+    res.set({
+      'Content-Type': 'application/vnd.apple.mpegurl',
+      'Cache-Control': response.headers['cache-control'],
+    });
+    response.data.pipe(res);
+  } catch (error: any) {
+    console.log(error.message);
+  }
+};
