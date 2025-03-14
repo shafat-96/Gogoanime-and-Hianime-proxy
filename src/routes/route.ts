bfcfd14b62a0ca75409d779abe3661827d209ab1
julz <owanojulius742002@gmail.com>
2025-03-14 20:55:26 +0800

    Back to  Remove origin in proxy headers

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,24 +1,6 @@
 import express from 'express';
-import { hlsProxy, qualityProxy, segmentProxy } from '../controllers/proxy';
-import { hianimeHlsProxy, hianimeQaulityProxy, hianimeSegmentProxy } from '../controllers/hianime-proxy';
-import { m3u8Proxy, m3u8QualityProxy, m3u8SegmentProxy } from '../controllers/m3u8-proxy';
+import { m3u8Proxy } from '../controllers/m3u8-proxy';
 
 export const router = express.Router();
 
-router.get('/hls-proxy', hlsProxy);
-
-router.get('/quality-proxy', qualityProxy);
-
-router.get('/segment-proxy', segmentProxy);
-
-router.get('/hianime-hls-proxy', hianimeHlsProxy);
-
-router.get('/hianime-quality-proxy', hianimeQaulityProxy);
-
-router.get('/hianime-segment-proxy', hianimeSegmentProxy);
-
 router.get('/m3u8-proxy', m3u8Proxy);
-
-router.get('/m3u8-quality-proxy', m3u8QualityProxy);
-
-router.get('/m3u8-segment-proxy', m3u8SegmentProxy);
